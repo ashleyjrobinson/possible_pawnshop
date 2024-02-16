9e0716e10f497a8652ed95694c9665f4c4b9604e
ashleyjrobinson <ashjrobinson95@gmail.com>
2024-02-16 08:50:11 +0000

    refactor: :sparkles: Allow for config use of SET_BLIP_AS_SHORT_RANGE

--- shared/config.lua
+++ shared/config.lua
@@ -18,12 +18,14 @@ return {
             sprite = 431,
             color = 5,
             coords = vector3(412.21, 315.15, 103.13),
+            shortRange = true,
         },
         {
             label = 'Pawnshop South',
             sprite = 431,
             color = 5,
             coords = vector3(182.39, -1319.46, 29.32),
+            shortRange = true,
         }
     },
     sellItems = {
